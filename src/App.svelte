df8f0d525a192944dff155b62889bcb3a507f835
DiogoCDev <98128514+DiogoCDev@users.noreply.github.com>
2022-02-01 22:44:08 +0000

    Initial Commit

---
+++ src/App.svelte
@@ -0,0 +1,78 @@
+<script lang="ts">
+	//export let name: string;
+	import router from "page";
+	import Home from "./pages/Home.svelte";
+	import Contact from "./pages/Contact.svelte";
+	import About from "./pages/About.svelte";
+	import Services from "./pages/Services.svelte";
+	let page;
+
+	router("/", () => (page = Home));
+	router("/services", () => (page = Services));
+	router("/about", () => (page = About));
+	router("/contact", () => (page = Contact));
+
+	router.start();
+
+	function pageSwitch(pName: string) {
+		window.location.href = `/${pName}`;
+	}
+</script>
+
+<header>
+	<img src="assets/imgs/complogo.png" alt="" class="header-icon" on:click={() => pageSwitch("")}/>
+	<div class="header-btns">
+		<span on:click={() => pageSwitch("contact")} class="header-btns-contact">Contact</span>
+		<span on:click={() => pageSwitch("about")} class="header-btns-about">About Us</span>
+		<button class="header-btns-service" on:click={() => pageSwitch("services")}>Services</button>
+	</div>
+</header>
+<svelte:component this={page} />
+
+<style>
+	header {
+		background-color: rgb(61, 61, 61);
+		height: 5em;
+	}
+	.header-icon {
+		margin-left: 1.7em;
+		width: 95px;
+		height: 80px;
+	}
+	.header-btns {
+		margin-top: 0.7em;
+		float: right;
+		font-family: "Roboto Condensed", sans-serif;
+		margin-right: 6em;
+		font-size: 24px;
+	}
+	.header-btns-service, .header-btns-about, .header-btns-contact{
+		padding-right: 1em;
+	}
+	.header-btns-about:hover, .header-btns-contact:hover {
+		color: var(--primary-color);
+		transition-duration: 0.5s;
+	}
+	.header-btns-service {
+		display: inline-block;
+		padding: 10px;
+		font-size: 24px;
+		cursor: pointer;
+		text-align: center;
+		text-decoration: none;
+		outline: none;
+		color: #fff;
+		background-color: var(--primary-color);
+		border: none;
+		border-radius: 15px;
+	}
+
+	.header-btns-service:hover {
+		background-color: var(--primary-color);
+	}
+	.header-btns-service:active {
+		background-color: var(--primary-color);
+		transition-duration: 0.1s;
+		transform: translateY(-5px);
+	}
+</style>
---
+++ src/pages/Home.svelte
@@ -0,0 +1,38 @@
+<div style="display: flex;">
+    <div style="margin-left: 8vw;">
+        <div class="landing-title">
+            We do <span style="color: var(--primary-color);">IT</span> for you.
+        </div>
+        <div class="landing-subtitle">
+            Suporte de IT para o seu negócio?<br />
+            <span style="color: var(--primary-color);">Contacte-nos já!</span>
+        </div>
+    </div>
+
+    <img src="assets/imgs/home.png" alt="Home" class="landing-photo" /><br>
+</div>
+<br><br><br>
+<hr>
+<br>
+
+
+
+<style>
+    .landing-title {
+        font-size: 4.5vw;
+        font-family: "Roboto Condensed", sans-serif;
+        margin-top: 17vw;
+    }
+    .landing-subtitle {
+        font-size: 1.5vw;
+        font-family: "Roboto Condensed", sans-serif;
+    }
+    .landing-photo {
+        margin-left: auto;
+        margin-right: 3vw;
+        margin-top: 4vw;
+        width: 52vw;
+        height: 40vw;
+        max-width: 100%;
+    }
+</style>
